dd3973927400b15cf9c033f825de948c047029c3
James McLaughlin <jamie@lacewing-project.org>
2011-09-19 16:02:21 +0100

    lw_eventpump_start_sleepy_ticking in the C API had incorrect callback parameter

--- include/Lacewing.h
+++ include/Lacewing.h
@@ -139,7 +139,7 @@ LacewingFunction          void  lw_sha1_hex                 (char * output, cons
   LacewingFunction           void  lw_eventpump_delete               (lw_eventpump *);
   LacewingFunction           void  lw_eventpump_tick                 (lw_eventpump *);
   LacewingFunction           void  lw_eventpump_start_event_loop     (lw_eventpump *);
-  LacewingFunction           void  lw_eventpump_start_sleepy_ticking (lw_eventpump *, void (LacewingHandler * on_tick_needed) (lw_eventpump));
+  LacewingFunction           void  lw_eventpump_start_sleepy_ticking (lw_eventpump *, void (LacewingHandler * on_tick_needed) (lw_eventpump *));
   LacewingFunction           void  lw_eventpump_post_eventloop_exit  (lw_eventpump *);
   LacewingFunction        lw_bool  lw_eventpump_in_use               (lw_eventpump *);
   LacewingFunction           void  lw_eventpump_set_in_use           (lw_eventpump *, lw_bool);
--- src/c/eventpump_flat.cc
+++ src/c/eventpump_flat.cc
@@ -40,7 +40,7 @@ void lw_eventpump_tick (lw_eventpump * eventpump)
 void lw_eventpump_start_event_loop (lw_eventpump * eventpump)
     { ((Lacewing::EventPump *) eventpump)->StartEventLoop();
     }
-void lw_eventpump_start_sleepy_ticking (lw_eventpump * eventpump, void (LacewingHandler * on_tick_needed) (lw_eventpump))
+void lw_eventpump_start_sleepy_ticking (lw_eventpump * eventpump, void (LacewingHandler * on_tick_needed) (lw_eventpump *))
     { ((Lacewing::EventPump *) eventpump)->StartSleepyTicking((void (LacewingHandler *) (Lacewing::EventPump &)) on_tick_needed);
     }
 void lw_eventpump_post_eventloop_exit (lw_eventpump * eventpump)
