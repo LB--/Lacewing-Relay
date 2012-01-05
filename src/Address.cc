996b1b49d9c40da4c61802923ec57000cda77d72
James McLaughlin <jamie@lacewing-project.org>
2012-01-05 14:22:24 +0000

    Fix Address copy routine (it stored the original addrinfo instead of the copy)

--- src/Address.cc
+++ src/Address.cc
@@ -1,7 +1,7 @@
 
 /* vim: set et ts=4 sw=4 ft=cpp:
  *
- * Copyright (C) 2011 James McLaughlin.  All rights reserved.
+ * Copyright (C) 2011, 2012 James McLaughlin.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -249,7 +249,7 @@ Address::Address (Address &Address)
 
     memcpy (new_info->ai_addr, info->ai_addr, info->ai_addrlen);
 
-    internal->Info = internal->InfoToFree = info;
+    internal->Info = internal->InfoToFree = new_info;
 }
 
 Address::~Address ()
