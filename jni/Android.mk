fcf340acee2f2574e0ef53002aebd8edb343fc06
James <jamie@lacewing-project.org>
2013-03-22 15:10:05 +0000

    Update Android makefile/config

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,57 +2,65 @@ LOCAL_PATH := $(call my-dir)
   
 include $(CLEAR_VARS)
 
-LOCAL_LDLIBS := -llog -lssl -lcrypto
+LOCAL_LDLIBS := -llog
 
-LOCAL_CPP_EXTENSION := cc
+LOCAL_CPP_EXTENSION := .cc
 LOCAL_MODULE        := lacewing
 LOCAL_SRC_FILES     := \
-    ../src/Global.cc \
-    ../src/Sync.cc \
-    ../src/Filter.cc \
-    ../src/Address.cc \
-    ../src/StreamGraph.cc \
-    ../src/Stream.cc \
-    ../src/Pipe.cc \
-    ../src/Error.cc \
-    ../src/FlashPolicy.cc \
-	../src/Pump.cc \
-    ../src/webserver/Webserver.cc \
-    ../src/webserver/Sessions.cc \
-    ../src/webserver/Request.cc \
-    ../src/webserver/MimeTypes.cc \
-    ../src/webserver/http/HTTP.cc \
-    ../src/webserver/http/Multipart.cc \
-    ../deps/http-parser/http_parser.c \
-    ../src/unix/Event.cc \
-    ../src/unix/EventPump.cc \
-    ../src/unix/Server.cc \
-    ../src/unix/Timer.cc \
-    ../src/unix/UDP.cc \
-    ../src/unix/Client.cc \
-    ../src/unix/Thread.cc \
-    ../src/unix/File.cc \
-    ../src/unix/FDStream.cc \
-    ../src/c/addr_flat.cc \
-    ../src/c/pump_flat.cc \
-    ../src/c/stream_flat.cc \
-    ../src/c/pipe_flat.cc \
-    ../src/c/file_flat.cc \
-    ../src/c/eventpump_flat.cc \
-    ../src/c/global_flat.cc \
-    ../src/c/server_flat.cc \
-    ../src/c/timer_flat.cc \
-    ../src/c/webserver_flat.cc \
-    ../src/c/filter_flat.cc \
-    ../src/c/udp_flat.cc \
-    ../src/c/client_flat.cc \
-    ../src/c/error_flat.cc \
-    ../src/c/sync_flat.cc \
-    ../src/c/ssync_flat.cc \
-    ../src/c/thread_flat.cc \
-	../src/c/flashpolicy_flat.cc
+	../src/global.c \
+	../src/nvhash.c \
+	../src/filter.c \
+	../src/address.c \
+	../src/streamgraph.c \
+	../src/stream.c \
+	../src/error.c \
+	../src/webserver/webserver.c \
+	../src/webserver/upload.c \
+	../src/webserver/sessions.c \
+	../src/webserver/mimetypes.c \
+	../src/webserver/request.c \
+	../src/webserver/multipart.c \
+	../src/webserver/http/http-client.c \
+	../src/webserver/http/http-parse.c \
+	../deps/http-parser/http_parser.c \
+	../deps/multipart-parser/multipart_parser.c \
+	../src/pipe.c \
+	../src/flashpolicy.c \
+	../src/pump.c \
+	../src/util.c \
+	../src/list.c \
+	../src/heapbuffer.c \
+	../src/cxx/address.cc \
+	../src/cxx/client.cc \
+	../src/cxx/error.cc \
+	../src/cxx/event.cc \
+	../src/cxx/eventpump.cc \
+	../src/cxx/fdstream.cc \
+	../src/cxx/file.cc \
+	../src/cxx/filter.cc \
+	../src/cxx/flashpolicy.cc \
+	../src/cxx/pipe.cc \
+	../src/cxx/pump.cc \
+	../src/cxx/server.cc \
+	../src/cxx/stream.cc \
+	../src/cxx/sync.cc \
+	../src/cxx/thread.cc \
+	../src/cxx/timer.cc \
+	../src/cxx/udp.cc \
+	../src/unix/client.c \
+	../src/unix/event.c \
+	../src/unix/eventpump.c \
+	../src/unix/server.c \
+	../src/unix/thread.c \
+	../src/unix/timer.c \
+	../src/unix/udp.c \
+	../src/unix/fdstream.c \
+	../src/unix/file.c \
+	../src/unix/sync.c \
+	../src/unix/global.c
 
-LOCAL_CFLAGS := -DLacewingLibrary -fno-exceptions -fno-rtti -fvisibility=hidden -DHTTP_PARSER_STRICT=0 -DHTTP_PARSER_DEBUG=0 
+LOCAL_CFLAGS := -std=gnu99 -D_lacewing_library -D_lacewing_no_ssl -D_lacewing_no_spdy -fvisibility=hidden -DHTTP_PARSER_STRICT=1 -DHTTP_PARSER_DEBUG=0
+LOCAL_CXXFLAGS := -fno-exceptions -fno-rtti
 
 include $(BUILD_SHARED_LIBRARY)
 
--- jni/config.h
+++ jni/config.h
@@ -104,7 +104,7 @@
 #define PACKAGE_NAME "liblacewing"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "liblacewing 0.1.0"
+#define PACKAGE_STRING "liblacewing 0.5.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "liblacewing"
@@ -113,10 +113,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1.0"
+#define PACKAGE_VERSION "0.5.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.1.0"
+#define VERSION "0.5.1"
